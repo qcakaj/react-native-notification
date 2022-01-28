0f35e7eb5ae08e0faf65c0402472dc9ad7a35966
qcakaj <36235406+qcakaj@users.noreply.github.com>
2022-01-28 16:05:53 +0100

    chore: initial commit

---
+++ android/src/main/java/com/reactnativenotification/NotificationModule.kt
@@ -0,0 +1,24 @@
+package com.reactnativenotification
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class NotificationModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "Notification"
+    }
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Int, b: Int, promise: Promise) {
+    
+      promise.resolve(a * b)
+    
+    }
+
+    
+}
---
+++ ios/Notification.swift
@@ -0,0 +1,8 @@
+@objc(Notification)
+class Notification: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}
